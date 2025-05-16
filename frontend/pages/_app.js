82e37dba08677de3382ce51aee6bed6fead15a36
thermonuclearblastfurnace <rckearns@gmail.com>
2025-05-16 22:30:54 +0000

    feat: import Mapbox GL CSS in App entry

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,5 @@
 import 'mapbox-gl/dist/mapbox-gl.css'
-import '../styles/globals.css'
+import '../styles/global.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
